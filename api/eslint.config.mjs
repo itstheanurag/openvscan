7fe7c485aa38a5f2bdd89b7d5e1f530c131b99fc
gaurav <gauravanurag36@gmail.com>
2025-10-02 22:35:40 +0530

    feat: install the required packages

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -26,9 +26,25 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/ban-types': 'error',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: ['variable', 'function'],
+          format: ['camelCase', 'UPPER_CASE'],
+        },
+      ],
+      '@typescript-eslint/no-confusing-non-null-assertion': 'warn',
+      '@typescript-eslint/no-confusing-void-expression': 'warn',
+      '@typescript-eslint/no-for-in-array': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'prettier/prettier': 'error',
     },
   },
 );
