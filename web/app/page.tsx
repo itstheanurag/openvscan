e3f4615e7db5b18ec57c3ad1fae1682122334dca
buddhsen-tripathi <buddhsent19@gmail.com>
2025-10-09 08:36:42 -0400

    feat: removed redundant pages and added homepage components

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,13 @@
+import FeaturesGrid from "@/components/FeaturesGrid";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FeaturesGrid />
+      <Footer />
     </main>
   );
 }
