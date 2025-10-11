78b94c62b4a5c2992b83eb135cc9b65e6c7a7107
gaurav <gauravanurag36@gmail.com>
2025-10-11 19:36:26 +0530

    chore: remove the loggin code for the pool

--- api/src/database/drizzle.provider.ts
+++ api/src/database/drizzle.provider.ts
@@ -1,10 +1,8 @@
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import { Logger, Provider } from '@nestjs/common';
+import { Provider } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as schema from './models/schema';
-
-const drizzleLogger = new Logger('DrizzleORM');
 
 export const DATABASE_PROVIDER = 'DATABASE_PROVIDER';
 export const POOL_PROVIDER = 'POOL_PROVIDER';
@@ -22,32 +20,6 @@ export const PoolProvider: Provider = {
           ? { rejectUnauthorized: false }
           : false,
     });
-
-    const originalQuery = pool.query.bind(pool);
-    pool.query = async (...args: any[]) => {
-      const start = performance.now();
-      const queryText = args[0];
-      const queryParams = args[1];
-      try {
-        const result = await originalQuery(...args);
-        const duration = (performance.now() - start).toFixed(2);
-        drizzleLogger.debug(
-          `✅ SQL (${duration} ms): ${queryText}\nParams: ${JSON.stringify(
-            queryParams,
-          )}\nResult: ${JSON.stringify(result.rows, null, 2)}`,
-        );
-        return result;
-      } catch (error) {
-        const duration = (performance.now() - start).toFixed(2);
-        drizzleLogger.error(
-          `❌ SQL (${duration} ms): ${queryText}\nParams: ${JSON.stringify(
-            queryParams,
-          )}\nError: ${error.message}`,
-        );
-        throw error;
-      }
-    };
-
     return pool;
   },
 };
