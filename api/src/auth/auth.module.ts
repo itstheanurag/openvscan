b4c86610aa57c25de1e36dfca4517859ef1ed9bf
gaurav <gauravanurag36@gmail.com>
2025-10-27 12:36:02 +0530

    feat: integrate betterAuth with recommended @thallesp/nestjs-better-auth library

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthService } from './services/auth.service';
-import { AuthController } from './auth.controller';
-import { BetterAuthProvider } from './services';
+import { createBetterAuthOptions } from './options/better-auth.option';
+import { ConfigService } from '@nestjs/config';
+import { DATABASE_PROVIDER } from 'src/database/drizzle.provider';
+import { NeonHttpDatabase } from 'drizzle-orm/neon-http';
+import * as authSchema from 'src/database/models/auth-schema';
+import { DatabaseModule } from 'src/database/database.module';
+import { AuthModule as BetterAuthModule } from '@thallesp/nestjs-better-auth';
+import { betterAuth } from 'better-auth';
 
 @Module({
-  imports: [ConfigModule],
-  controllers: [AuthController],
-  providers: [BetterAuthProvider, AuthService],
-  exports: [AuthService],
+  imports: [
+    DatabaseModule,
+    BetterAuthModule.forRootAsync({
+      imports: [DatabaseModule],
+      inject: [ConfigService, DATABASE_PROVIDER],
+      useFactory: (
+        config: ConfigService,
+        db: NeonHttpDatabase<typeof authSchema>,
+      ) => ({
+        auth: betterAuth(createBetterAuthOptions(config, db)),
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
--- api/src/auth/types/index.ts
+++
@@ -1,3 +0,0 @@
-import { type Request } from 'express';
-
-export type ExpressRequest = Request;
